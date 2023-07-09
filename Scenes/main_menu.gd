674bc87c0e34f0dd47789233a41120848c311e10
FinixGameDev <73645385+FinixGameDev@users.noreply.github.com>
2023-07-09 17:21:19 +0100

    Final Build

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -6,7 +6,7 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _input(event):
-	if event is InputEventKey:
+	if event is InputEventKey or event is InputEventJoypadButton:
 		if event.pressed:
 			var scene = main_scene.instantiate()
 			get_parent().add_child(scene)
